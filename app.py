ecba50b7e5e90f068e8f5cd8a150262fa079ad01
wojtekswiderski <wswiders@uwaterloo.ca>
2015-09-05 19:57:10 -0400

    Simple file management

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_from_directory
+from os import listdir
 
 from learn import Learn
 from tomidi import toMidi
@@ -7,7 +8,7 @@ from midi2mp3 import generateMP3
 import json
 
 MIDI_TEMP_PATH = 'songs/output.mid'
-MP3_TEMP_PATH = 'songs/output.mp3'
+MP3_BASE_PATH = 'songs/output'
 FONT_PATH = 'Gort\'s-DoubleDecker_J1.SF2'
 BASE_URL = 'http://127.0.0.1:5000/'
 app = Flask(__name__)
@@ -21,8 +22,9 @@ def music_function():
     learner.loadFromFile()
     song = learner.getSong(getStartSequence())
     toMidi(song, MIDI_TEMP_PATH)
-    generateMP3(MIDI_TEMP_PATH, MP3_TEMP_PATH, FONT_PATH)
-    return BASE_URL + MP3_TEMP_PATH
+    mp3Path = getMP3Path()
+    generateMP3(MIDI_TEMP_PATH, mp3Path, FONT_PATH)
+    return BASE_URL + mp3Path
 
 @app.route('/train')
 def train_function():
@@ -30,9 +32,6 @@ def train_function():
     learner.train()
     learner.saveToFile()
     return server()
-
-def getStartSequence():
-    return [0, 7, 9, 0]
 
 @app.route('/js/<path:path>')
 def send_js(path):
@@ -49,6 +48,19 @@ def send_css(path):
 @app.route('/songs/<path:path>')
 def send_songs(path):
     return send_from_directory('songs', path)
+
+def getStartSequence():
+    return [0, 7, 9, 0]
+
+def getMP3Path():
+    files = listdir('songs')
+    files = [('songs/' + x) for x in files]
+    i = 0
+    while True:
+        songName = MP3_BASE_PATH + str(i) + '.mp3'
+        if not songName in files:
+            return songName
+        i += 1
 
 if __name__ == "__main__":
     app.run(debug = True)
