42ad7bb696924962f90f06b62659034ee47aa3fb
wojtekswiderski <wswiders@uwaterloo.ca>
2015-09-05 19:00:40 -0400

    Generate mp3 on endpoints

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, send_from_directory
 
 from learn import Learn
 from tomidi import toMidi
+from midi2mp3 import generateMP3
 
 import json
 
 MIDI_TEMP_PATH = 'output.mid'
+MP3_TEMP_PATH = 'output.mp3'
+FONT_PATH = 'Gort\'s-DoubleDecker_J1.SF2'
 
 app = Flask(__name__)
 @app.route('/')
@@ -18,7 +21,7 @@ def music_function():
     learner.loadFromFile()
     song = learner.getSong(getStartSequence())
     toMidi(song, MIDI_TEMP_PATH)
-
+    generateMP3(MIDI_TEMP_PATH, MP3_TEMP_PATH, FONT_PATH)
     return json.dumps(song)
 
 @app.route('/train')
