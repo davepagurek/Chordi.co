0aefc937b64458397ef37af71a309f450ee70dc6
Dave Pagurek <dave@davepagurek.com>
2015-09-05 00:13:11 -0400

    Make into class

--- parse.py
+++ parse.py
@@ -1,23 +1,50 @@
 import midi
 
-pattern = midi.read_midifile("Midi Thingies/twinkle.mid")
-tempo = 120
-song = []
+class Song:
+    notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"]
+    def __init__(self, filename):
+        self.song = []
+        self.tempo = 120
 
-def dataToBPM(data):
-    print data
-    timePerQuarter = ""
-    for digit in data:
-        timePerQuarter += hex(digit)[2:]
-    return 60000000/int(timePerQuarter, 16)
+        pattern = midi.read_midifile("Midi Thingies/twinkle.mid")
 
-for track in pattern:
-    currentNotes = []
-    for event in track:
-        if type(event) is midi.SetTempoEvent:
-            tempo = dataToBPM(event.data)
-            print "TEMPO: ", tempo
-        if type(event) is midi.NoteOnEvent:
-            print ((event.data[0]-21)%12 - 3) % 12
+        for track in pattern:
+            lastTick = -1
+            currentTick = 0
+            currentNotes = set()
+            for event in track:
+                if type(event) is midi.SetTempoEvent:
+                    self.tempo = __dataToBPM(event.data)
+
+                if type(event) is midi.NoteOnEvent:
+                    set.add(__noteFromMidi(event.data[0]))
+                    currentTick += event.tick
+
+                if type(event) is midi.NoteOffEvent:
+                    set.remove(__noteFromMidi(event.data[0]))
+                    currentTick += event.tick
+
+                if currentTick != lastTick:
+                    self.song += {from: lastTick+1, to: currentTick, notes: currentNotes.copy()}
+                    lastTick = currentTick
+
+    def __dataToBPM(data):
+        print data
+        timePerQuarter = ""
+        for digit in data:
+            timePerQuarter += hex(digit)[2:]
+        return 60000000/int(timePerQuarter, 16)
+
+    def __noteFromMidi(num):
+        index = ((num-21)%12 - 3) % 12
+        if 0 <= index < len(notes):
+            return notes[index]
+        else:
+            return "undef: " + str(index)
+
+
+
+twinkle = Song("Midi Thingies/twinkle.mid")
+print twinkle.song
+
