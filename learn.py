5c9c59db54fe8e1e1153739af647d5461a95bacc
wojtekswiderski <wswiders@uwaterloo.ca>
2015-09-05 14:32:23 -0400

    Learn.py now oop

--- learn.py
+++ learn.py
@@ -17,82 +17,97 @@ import random
 import pdb
 import static
 
-n = FeedForwardNetwork()
+class Learn:
 
-dataModel = SongFactory().getModels()
+    def __init__(self):
+        self.net = None
 
-ds = SupervisedDataSet(static.NUM_OF_INPUTS, 1)
+    def train(self):
 
-#adds samples from the data received from songfactory and the k
-for data in dataModel:
-    for input, target in data.model:
-        print input, target
-        ds.addSample(input, target)
+        n = FeedForwardNetwork()
+
+        dataModel = SongFactory().getModels()
+
+        ds = SupervisedDataSet(static.NUM_OF_INPUTS, 1)
+
+        #adds samples from the data received from songfactory and the k
+        for data in dataModel:
+            for input, target in data.model:
+                print input, target
+                ds.addSample(input, target)
 
 
-#instantiate the network
-net = FeedForwardNetwork()
-bias = BiasUnit()
-net.addModule(bias)
+        #instantiate the network
+        self.net = FeedForwardNetwork()
+        bias = BiasUnit()
+        self.net.addModule(bias)
 
-#create the layers of the network
-inLayer = LinearLayer(static.NUM_OF_INPUTS)
-outLayer = LinearLayer(1)
-hidden1 = SigmoidLayer(25)
-hidden2 = SigmoidLayer(5)
+        #create the layers of the network
+        inLayer = LinearLayer(static.NUM_OF_INPUTS)
+        outLayer = LinearLayer(1)
+        hidden1 = SigmoidLayer(25)
+        hidden2 = SigmoidLayer(5)
 
-#add the layers
-net.addInputModule(inLayer)
-net.addOutputModule(outLayer)
-net.addModule(hidden1)
-net.addModule(hidden2)
+        #add the layers
+        self.net.addInputModule(inLayer)
+        self.net.addOutputModule(outLayer)
+        self.net.addModule(hidden1)
+        self.net.addModule(hidden2)
 
-#create the connection
-in_h1 = FullConnection(inLayer,hidden1)
-h1_h2 = FullConnection(hidden1, hidden2)
-h2_out = FullConnection(hidden2, outLayer)
-b_h1  = FullConnection(bias, hidden1)
-b_h2  = FullConnection(bias, hidden2)
+        #create the connection
+        in_h1 = FullConnection(inLayer,hidden1)
+        h1_h2 = FullConnection(hidden1, hidden2)
+        h2_out = FullConnection(hidden2, outLayer)
+        b_h1  = FullConnection(bias, hidden1)
+        b_h2  = FullConnection(bias, hidden2)
 
-#add the connection
-net.addConnection(in_h1)
-net.addConnection(h1_h2)
-net.addConnection(h2_out)
-net.addConnection(b_h1)
-net.addConnection(b_h2)
+        #add the connection
+        self.net.addConnection(in_h1)
+        self.net.addConnection(h1_h2)
+        self.net.addConnection(h2_out)
+        self.net.addConnection(b_h1)
+        self.net.addConnection(b_h2)
 
-net.sortModules()
+        self.net.sortModules()
 
-#trainer to edit the network
-trainer = BackpropTrainer(net, ds, learningrate = 0.003)
+        #trainer to edit the network
+        trainer = BackpropTrainer(self.net, ds, learningrate = 0.003)
 
-trainer.trainEpochs(25)
-#generate a song given an input sequence
-def getSong(inputSequence):
-    inputSequence = [x for x in inputSequence]
-    song = [str(inputSequence[x])  for x in range(0, static.NUM_OF_INPUTS)]
-    nextout = 0
-    for x in range(0, 64):
-        nextout = int(net.activate(tuple(inputSequence)))
+        trainer.trainEpochs(25)
 
-        # just to shake it up a little if we get 4 of the same chord in a row
-        if nextout == inputSequence[-1] and nextout == inputSequence[-2] and nextout == inputSequence[-3]:
-            recurring = dict()
-            for i in song:
-                if i in recurring:
-                    recurring[i] += 1
-                else:
-                    recurring[i] = 1
+    #generate a song given an input sequence
+    def getSong(self, inputSequence):
+        inputSequence = [x for x in inputSequence]
+        song = [str(inputSequence[x])  for x in range(0, static.NUM_OF_INPUTS)]
+        nextout = 0
+        for x in range(0, 64):
+            nextout = int(self.net.activate(tuple(inputSequence)))
 
-            nextout = int(min(recurring, key = recurring.get))
+            # just to shake it up a little if we get 4 of the same chord in a row
+            if nextout == inputSequence[-1] and nextout == inputSequence[-2] and nextout == inputSequence[-3]:
+                recurring = dict()
+                for i in song:
+                    if i in recurring:
+                        recurring[i] += 1
+                    else:
+                        recurring[i] = 1
 
-        song.append(str(nextout))
-        inputSequence = inputSequence[1:]
-        inputSequence.append(nextout)
+                nextout = int(min(recurring, key = recurring.get))
 
-    print song[4:]
-    f = open('output.txt', 'w')
-    f.write(' '.join(song[4:]))
-    f.close()
+            song.append(str(nextout))
+            inputSequence = inputSequence[1:]
+            inputSequence.append(nextout)
 
-getSong([7, 9, 7, 9])
+        print song[4:]
+        f = open('output.txt', 'w')
+        f.write(' '.join(song[4:]))
+        f.close()
+
+def run():
+
+    learner = Learn()
+    learner.train()
+    learner.getSong([0, 7 ,9 ,0])
+
+if __name__ == "__main__":
+    run()
