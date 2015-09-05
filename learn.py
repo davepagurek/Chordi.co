3c9259053de4f2239e039ac9f2df795362337339
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-04 22:08:58 -0400

    Added function definition to return midi data.

--- learn.py
+++ learn.py
@@ -5,8 +5,7 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 
 dataModel = [
-    [(0,0,0), (0,)],
-    [(1,1,1), (1,)],
+    [(1,1,1,1,1), (1,)],
 ]
 
 ds = SupervisedDataSet(3, 1)
@@ -15,13 +14,10 @@ for input, target in dataModel:
 
 import random
 random.seed()
-trainingSet = SupervisedDataSet(3, 1);
-for ri in range(0,1000):
-    input,target = dataModel[random.getrandbits(2)];
-    trainingSet.addSample(input, target)
+trainingSet = SupervisedDataSet(5, 1);
 
 from pybrain.tools.shortcuts import buildNetwork
-net = buildNetwork(3, 10, 1, bias=True)
+net = buildNetwork(5, 10, 1, bias=True)
 
 from pybrain.supervised.trainers import BackpropTrainer
 trainer = BackpropTrainer(net, ds, learningrate = 0.001, momentum = 0.99)
@@ -30,3 +26,6 @@ trainer.trainEpochs(1000)
 
 for x in dataModel:
     print x[0],"-->",round(net.activate(x[0]))
+
+def getSong():
+    return "midi yay"
