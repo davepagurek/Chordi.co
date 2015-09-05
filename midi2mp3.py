67d0448b3967ef290495ba318b608759c9936c0e
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-05 17:54:11 -0400

    Moved sound font to new folder.

--- midi2mp3.py
+++ midi2mp3.py
@@ -1,6 +1,8 @@
 import subprocess
+import pdb
 def generateMP3(inputpath, outputpath, fontpath):
     subprocess.call(["fluidsynth", "-F","output.wav",fontpath, inputpath])
+    pdb.set_trace()
     subprocess.call(["lame", "output.wav", outputpath])
-    #subprocess.call(["rm","output.wav"])
-generateMP3("output.mid","output.mp3","~/Downloads/Gorts_DoubleDecker3/Gort\'s-DoubleDecker_J1.SF2")
+    subprocess.call(["rm","output.wav"])
+generateMP3("output.mid","output.mp3","Gort\'s-DoubleDecker_J1.SF2")
