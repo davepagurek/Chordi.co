600c704a411d934417d1aed63c8e733116d8a0b0
wojtekswiderski <wswiders@uwaterloo.ca>
2015-09-04 23:18:59 -0400

    Song Model constructor done

--- model.py
+++ model.py
@@ -2,21 +2,21 @@ class SongModel:
 	def __init__(self, songString, preChords = 5):
 
 		# list that contains enumerated chords
-		chords = songString.split(' ')
+		numbers = songString.split(' ')
+		chords = [int(x) for x in numbers]
 		if len(chords) < preChords + 1:
 			print 'Not enough chords in song'
 
 		i = 0
 		self.model = list()
+		finalChord = -1
 
-		while(finalChords <= 13):
+		while True:
 			finalChord = chords[i + preChords]
-			dataPoint = tuple(x for x in chord[i : i + preChords])
-
+			dataPoint = tuple(x for x in chords[i : i + preChords])
 			self.model.append([dataPoint, (finalChord,)])
+
+			if (finalChord > 13):
+				break
+
 			i += 1
-
-		self.printModel()
-
-	def printModel(self):
-		print self.model
